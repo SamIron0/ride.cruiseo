5e60bc43bce41be24fc23ce6928bafbb0d515562
sam <ironkwec@myumanitoba.ca>
2023-12-11 22:02:52 -0600

    nonzoom

--- components/ui/carpool-form/index.tsx
+++ components/ui/carpool-form/index.tsx
@@ -145,13 +145,6 @@ export function CarpoolForm({ user }: CarpoolFormProps) {
           <input
             value={origin}
             onChange={e => setOrigin(e.target.value)}
-            name="prompt"
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                e.preventDefault()
-                submitRef.current?.click()
-              }
-            }}
             placeholder="Enter an Origin"
             className="bg-transparent text-white placeholder:text-gray-400 ring-0 outline-none resize-none py-4 px-2 font-mono text-sm h-10 w-full transition-all duration-300"
           />
@@ -163,10 +156,7 @@ export function CarpoolForm({ user }: CarpoolFormProps) {
           </div>
         }
         <div className="bg-black mt-5 rounded-xl shadow-lg h-fit flex px-1 w-full ">
-          <input
-            placeholder="Enter a Destination"
-
-          ></input>
+          
           <input
             value={destination}
             onChange={e => setDestination(e.target.value)}
