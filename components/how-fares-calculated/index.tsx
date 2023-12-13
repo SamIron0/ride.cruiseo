31b1db3a2ada442869d11b0b75fd38c29b0e2e2c
sam <ironkwec@myumanitoba.ca>
2023-12-13 16:32:29 -0600

    fe

--- components/how-fares-calculated/index.tsx
+++ components/how-fares-calculated/index.tsx
@@ -15,12 +15,12 @@ export function HowFaresCalculated() {
                     <div className="flex pb-1">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide pt-1.5 lucide-target"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="12" r="6" /><circle cx="12" cy="12" r="2" />
                         </svg>
-                        <p>  You pay for only your seat, so you could be paying as low as 1/3 of a regular Uber ride if match with 2 other riders</p>
+                        <p className="pl-1">  You pay for only your seat, so you could be paying as low as 1/3 of a regular Uber ride if match with 2 other riders</p>
                     </div>
                     <div className="flex pb-1">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide pt-1.5 lucide-target"><circle cx="12" cy="12" r="10" /><circle cx="12" cy="12" r="6" /><circle cx="12" cy="12" r="2" />
                         </svg>
-                        <p> The maximum amount you will pay if not matched with other riders is only 90% of a regular Uber ride</p>
+                        <p className="pl-1"> The maximum amount you will pay if not matched with other riders is only 90% of a regular Uber ride</p>
                     </div>
                 </div>
             </div>
