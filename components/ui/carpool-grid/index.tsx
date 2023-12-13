97e8cfed7e3fc20dd17c0b81f0be04e2091b204a
sam <ironkwec@myumanitoba.ca>
2023-12-13 17:15:46 -0600

    done.

--- components/ui/carpool-grid/index.tsx
+++ components/ui/carpool-grid/index.tsx
@@ -23,7 +23,7 @@ export function CarpoolGrid({ user, trips }: CarpoolGridProps) {
 
 
   return (
-    <div className="animate-in fade-in mt-36 slide-in-from-bottom-4 duration-1200 w-full ease-in-out">
+    <div className="animate-in fade-in p-8 sm:p-12  mt-36 slide-in-from-bottom-4 duration-1200 w-full ease-in-out">
       <h2 className="font-semibold text-md text-left w-full mb-3">My Trips</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 justify-items-stretch w-full">
         {usersTrips?.map((trip) => (
