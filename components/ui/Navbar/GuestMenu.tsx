2831cccb8010cc19500d55b830205a5f7ed1df57
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-12-14 10:21:47 -0600

    bugs

--- components/ui/Navbar/GuestMenu.tsx
+++ components/ui/Navbar/GuestMenu.tsx
@@ -74,7 +74,6 @@ export default function GuestMenu() {
                 {isOpen ?
                     <div id="dropdownInformation" className="z-10 mt-14 sm:mr-8 mr-6 absolute top-0 right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                         <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                            <div>{userName}</div>
                             <div className="font-medium truncate">Welcome</div>
                         </div>
                         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
