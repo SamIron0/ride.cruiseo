8d5cdc1b7f5c50995247d52c9ce5b2bf3950185a
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-12-14 10:44:36 -0600

    test

--- components/ui/carpool-card/index.tsx
+++ components/ui/carpool-card/index.tsx
@@ -36,7 +36,7 @@ export function CarpoolCard({ trip }: CarpoolCardProps) {
   return (
     <div className="bg-gray-200 p-2 border border-1 rounded-md ">
       <div className="flex items-center h-full space-x-4 rtl:space-x-reverse">
-        <div className="flex-1 min-w-0">
+        <div className="flex-1 w-7/10">
           <p className="text-sm flex  font-semibold text-gray-900 truncate dark:text-white">
             Origin: <p className="text-sm text-gray-500 truncate dark:text-gray-400">
               {trip.origin}</p>
@@ -45,12 +45,12 @@ export function CarpoolCard({ trip }: CarpoolCardProps) {
             Destination: <p className="text-sm  text-gray-500 truncate dark:text-gray-400">
               {trip.destination}</p>
           </p>
-          <p className="text-sm flex font-semibold fle text-gray-900 truncate dark:text-white">
+          <p className="text-sm flex font-semibold text-gray-900 truncate dark:text-white">
             Price: <p className="text-sm   text-gray-500 truncate dark:text-gray-400">
               {trip.price}</p>
           </p>
           <p className="text-sm flex font-semibold text-gray-900 truncate dark:text-white">
-            Dropoff: <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+            Pickup: <p className="text-sm text-gray-500 truncate dark:text-gray-400">
               {trip.date}</p>
           </p>
         </div>
