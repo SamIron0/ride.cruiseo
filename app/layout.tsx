587f064732019b92234732ef3e305cbf79f9696d
sam <ironkwec@myumanitoba.ca>
2023-12-17 09:35:03 -0600

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSKBCKW7SS"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LSKBCKW7SS');
+          `}
+        </script>
+
+
+
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LSKBCKW7SS"
