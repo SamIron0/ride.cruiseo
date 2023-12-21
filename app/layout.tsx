c4932f2f0619468bfdaecdfd65732d929b57294c
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-12-20 18:28:40 -0600

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,16 +47,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LSKBCKW7SS"></script>
-        <script>{
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){
-            dataLayer.push(arguments)
-          }
-          gtag('js', new Date());
+        <script
+          async src="https://www.googletagmanager.com/gtag/js?id=G-LSKBCKW7SS"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-LSKBCKW7SS');
+              `,
+          }}
+        ></script>
 
-          gtag('config', 'G-LSKBCKW7SS');}
-        </script>
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
