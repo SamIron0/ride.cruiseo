48f26e27f1f2e2adca94a949b14d7797411a037c
sam <samuelironkwec@gmail.com>
2023-12-25 15:58:40 -0600

    ee

--- components/ui/carpool-form/index.tsx
+++ components/ui/carpool-form/index.tsx
@@ -92,7 +92,7 @@ export const CarpoolForm = ({ user, onClose }: CarpoolFormProps) => {
   const requestButton = () => {
     if (!user) {
       return (<>
-        <Link href="/signin" className="inline-flex mt-8 items-center px-[98px] py-2 text-sm font-medium  border rounded-lg  bg-fuchsia-600 text-white border-fuchsia-400	 hover:text-white hover:bg-fuchsia-500">Request <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+        <Link href="/signin" className="inline-flex mt-8 items-center px-[98px] py-3 w-full text-sm font-medium  border rounded-lg  bg-fuchsia-600 text-white border-fuchsia-400	 hover:text-white hover:bg-fuchsia-500">Request <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
         </svg>
         </Link>
       </>)
