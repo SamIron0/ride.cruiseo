6250711e352de2c40e0ce206a6b40aeb944f271c
sam <samuelironkwec@gmail.com>
2023-12-25 19:09:08 -0600

    s

--- components/ui/carpool-form/index.tsx
+++ components/ui/carpool-form/index.tsx
@@ -304,7 +304,7 @@ export const CarpoolForm = ({ user, onClose }: CarpoolFormProps) => {
                 className="flex flex-col mb-4 border-gray-300  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
                 <div className="w-full">
                   <h1 className="font-mono font-bold  text-black text-lg ">From where?</h1>
-                  <div className="relative">
+                  <div className="mt-2  relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                       <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
