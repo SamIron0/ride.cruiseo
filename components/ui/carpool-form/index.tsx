f02d71b43b5854df970d7303d9bd9756a3d760d9
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2023-12-28 17:02:50 -0600

    gg

--- components/ui/carpool-form/index.tsx
+++ components/ui/carpool-form/index.tsx
@@ -321,8 +321,11 @@ export const CarpoolForm = ({ user, onClose, selectedDestination }: CarpoolFormP
                       {formattedOriginOptions?.map((formatOption, index) => (
                         <button
                           onClick={() => onOriginSuggestionClick(formatOption.value)}
-                          className="text-md hover:bg-gray-100 text-left w-full p-1"
-                          key={index}>{formatOption.value}</button>
+                          className="text-md hover:bg-gray-100 flex items-center text-left w-full p-1"
+                          key={index}>
+                          <div className="bg-gray-100 flex rounded-xl justify-center items-center p-2 mr-3"> <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26"><path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" /></svg></div>
+                          {formatOption.value}
+                        </button>
                       ))}
                     </div>
                   }
