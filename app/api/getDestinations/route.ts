765f7bd8ea8dcda22d6cd4ef71ec38dce5c506e7
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2023-12-30 18:26:22 -0600

    bug fix

--- app/api/getDestinations/route.ts
+++ app/api/getDestinations/route.ts
@@ -1,9 +1,9 @@
-
+import { createRouteHandlerClient, createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Trip } from "@/types";
+import { Database } from "@/types_db";
+import { cookies } from "next/headers";
+import { createTrip, deleteTrip } from "@/utils/supabase-admin";
 import { retrieveDestinations } from "@/utils/supabase-admin";
-
-class GeoCoordinate {
-    constructor(public latitude: number, public longitude: number) { }
-}
 
 function calculateHaversineDistance(coord1: GeoCoordinate, coord2: GeoCoordinate): number {
     const earthRadius = 6371; // Earth's radius in kilometers
@@ -25,35 +25,53 @@ function toRadians(degrees: number): number {
     return degrees * (Math.PI / 180);
 }
 
-export async function POST(req: Request) {
+export async function POST(req: Request,) {
+    let location = ''
     if (req.method === 'POST') {
         try {
-            // Fetch user location
-            const res = await fetch('/api/getLocation');
-            if (res.status === 200) {
-                const data = await res.json();
-                const userLocation = new GeoCoordinate(data.latitude, data.longitude);
-
-                // Example usage:
-                const destinationPoint = new GeoCoordinate(34.0522, -118.2437); // Los Angeles, CA
-
-                const distance = calculateHaversineDistance(userLocation, destinationPoint);
-                console.log(`The distance between the two points is approximately ${distance.toFixed(2)} kilometers.`);
-
-                // Retrieve destinations based on the user's location
-                const destinations = await retrieveDestinations(data.location.region_name);
-
-                if (destinations) {
-                    // Filter destinations or perform other logic
-                    return new Response(JSON.stringify(destinations), { status: 200 });
+            const fetchLocation = async () => {
+                try {
+                    const res = await fetch('/api/getLocation');
+                    if (res.status === 200) { // valid response
+                        const data = await res.json();
+                        location = data.location.region_name;
+                    } else {
+                        console.error("An error occurred while fetching the location");
+                    }
+                } catch (error) {
+                    console.error("An error occurred while fetching the location:", error);
                 }
-            } else {
-                console.error("An error occurred while fetching the location");
+            };
+
+
+            class GeoCoordinate {
+                constructor(public latitude: number, public longitude: number) { }
             }
+
+
+            // Example usage:
+            const point1 = new GeoCoordinate(37.7749, -122.4194); // San Francisco, CA
+            const point2 = new GeoCoordinate(34.0522, -118.2437); // Los Angeles, CA
+
+            const distance = calculateHaversineDistance(point1, point2);
+            console.log(`The distance between the two points is approximately ${distance.toFixed(2)} kilometers.`);
+
+            const destinations = await retrieveDestinations(location);
+            let response;
+            // filter destinations to only give contain destinations based on users location
+
+            if (destinations != undefined) {
+                response = destinations;
+                return new Response(JSON.stringify(response), {
+                    status: 200
+                });
+            }
+
         } catch (err: any) {
             return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
         }
-    }
+    } else {
+        return new Response(JSON.stringify('Method Not Allowed'));
 
-    return new Response(JSON.stringify('Method Not Allowed'), { status: 405 });
+    }
 }
