e8668494d8e1240b37094a688ec0415ed7cf3442
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2023-12-30 18:16:25 -0600

    gg

--- app/api/getDestinations/route.ts
+++ app/api/getDestinations/route.ts
@@ -1,78 +1,60 @@
-import { createRouteHandlerClient, createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { Trip } from "@/types";
-import { Database } from "@/types_db";
-import { cookies } from "next/headers";
-import { createTrip, deleteTrip } from "@/utils/supabase-admin";
+import { Request } from 'express'; // Assuming Request is imported from express
+
 import { retrieveDestinations } from "@/utils/supabase-admin";
 
+class GeoCoordinate {
+    constructor(public latitude: number, public longitude: number) {}
+}
+
 function calculateHaversineDistance(coord1: GeoCoordinate, coord2: GeoCoordinate): number {
-                const earthRadius = 6371; // Earth's radius in kilometers
+    const earthRadius = 6371; // Earth's radius in kilometers
 
-                const dLat = toRadians(coord2.latitude - coord1.latitude);
-                const dLon = toRadians(coord2.longitude - coord1.longitude);
+    const dLat = toRadians(coord2.latitude - coord1.latitude);
+    const dLon = toRadians(coord2.longitude - coord1.longitude);
 
-                const a =
-                    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                    Math.cos(toRadians(coord1.latitude)) * Math.cos(toRadians(coord2.latitude)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(coord1.latitude)) * Math.cos(toRadians(coord2.latitude)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
-                const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-                const distance = earthRadius * c; // Distance in kilometers
-                return distance;
-            }
+    const distance = earthRadius * c; // Distance in kilometers
+    return distance;
+}
 
-            function toRadians(degrees: number): number {
-                return degrees * (Math.PI / 180);
-            }
+function toRadians(degrees: number): number {
+    return degrees * (Math.PI / 180);
+}
 
-export async function POST(req: Request,) {
-    let location = ''
+export async function POST(req: Request) {
     if (req.method === 'POST') {
         try {
-            const fetchLocation = async () => {
-                try {
-                    const res = await fetch('/api/getLocation');
-                    if (res.status === 200) { // valid response
-                        const data = await res.json();
-                        location = data.location.region_name;
-                        // setLocationFetched(true); // Mark location as fetched
-                    } else {
-                        console.error("An error occurred while fetching the location");
-                    }
-                } catch (error) {
-                    console.error("An error occurred while fetching the location:", error);
+            // Fetch user location
+            const res = await fetch('/api/getLocation');
+            if (res.status === 200) {
+                const data = await res.json();
+                const userLocation = new GeoCoordinate(data.latitude, data.longitude);
+
+                // Example usage:
+                const destinationPoint = new GeoCoordinate(34.0522, -118.2437); // Los Angeles, CA
+
+                const distance = calculateHaversineDistance(userLocation, destinationPoint);
+                console.log(`The distance between the two points is approximately ${distance.toFixed(2)} kilometers.`);
+
+                // Retrieve destinations based on the user's location
+                const destinations = await retrieveDestinations(data.location.region_name);
+
+                if (destinations) {
+                    // Filter destinations or perform other logic
+                    return new Response(JSON.stringify(destinations), { status: 200 });
                 }
-            };
-
-
-            class GeoCoordinate {
-                constructor(public latitude: number, public longitude: number) { }
+            } else {
+                console.error("An error occurred while fetching the location");
             }
-
-            
-            // Example usage:
-            const point1 = new GeoCoordinate(37.7749, -122.4194); // San Francisco, CA
-            const point2 = new GeoCoordinate(34.0522, -118.2437); // Los Angeles, CA
-
-            const distance = calculateHaversineDistance(point1, point2);
-            console.log(`The distance between the two points is approximately ${distance.toFixed(2)} kilometers.`);
-
-            const destinations = await retrieveDestinations(location);
-            let response;
-            // filter destinations to only give contain destinations based on users location
-            
-            if (destinations != undefined) {
-                response = destinations;
-                return new Response(JSON.stringify(response), {
-                    status: 200
-                });
-            }
-
         } catch (err: any) {
             return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
         }
-    } else {
-        return new Response(JSON.stringify('Method Not Allowed'));
-
     }
+
+    return new Response(JSON.stringify('Method Not Allowed'), { status: 405 });
 }
