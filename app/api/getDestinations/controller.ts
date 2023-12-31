d038111f4d93d6981fdd82f4b10d9ea805a3abe0
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2023-12-31 14:28:28 -0600

    test

---
+++ app/api/getDestinations/controller.ts
@@ -0,0 +1,31 @@
+function calculateHaversineDistance(coord1: GeoCoordinate, coord2: GeoCoordinate): number {
+    const point1: GeoCoordinate = {
+        latitude: 37.7749,
+        longitude: -122.4194
+    };
+    const point2: GeoCoordinate = {
+        latitude: 34.0522,
+        longitude: -118.2437
+    };
+    const earthRadius = 6371; // Earth's radius in kilometers
+
+    const dLat = toRadians(coord2.latitude - coord1.latitude);
+    const dLon = toRadians(coord2.longitude - coord1.longitude);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(coord1.latitude)) * Math.cos(toRadians(coord2.latitude)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = earthRadius * c; // Distance in kilometers
+    return distance;
+}
+
+function toRadians(degrees: number): number {
+    return degrees * (Math.PI / 180);
+}
+export function filterDestinations() {
+    const distance = calculateHaversineDistance(point1, point2);
+
+}
--- app/api/getDestinations/route.ts
+++ app/api/getDestinations/route.ts
@@ -5,44 +5,12 @@ import { cookies } from "next/headers";
 import { createTrip, deleteTrip } from "@/utils/supabase-admin";
 import { retrieveDestinations } from "@/utils/supabase-admin";
 
-function calculateHaversineDistance(coord1: GeoCoordinate, coord2: GeoCoordinate): number {
-    const earthRadius = 6371; // Earth's radius in kilometers
-
-    const dLat = toRadians(coord2.latitude - coord1.latitude);
-    const dLon = toRadians(coord2.longitude - coord1.longitude);
-
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRadians(coord1.latitude)) * Math.cos(toRadians(coord2.latitude)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    const distance = earthRadius * c; // Distance in kilometers
-    return distance;
-}
-
-function toRadians(degrees: number): number {
-    return degrees * (Math.PI / 180);
-}
-
 export async function POST(req: Request) {
     const location = await req.json();
     if (req.method === 'POST') {
         try {
-
-            const point1: GeoCoordinate = {
-                latitude: 37.7749,
-                longitude: -122.4194
-            };
-            const point2: GeoCoordinate = {
-                latitude: 34.0522,
-                longitude: -118.2437
-            };
-
-            const distance = calculateHaversineDistance(point1, point2);
-            console.log(`The distance between the two points is approximately ${distance.toFixed(2)} kilometers.`);
-
-            const destinations = await retrieveDestinations(location ? location : "");
+    const destinations = await retrieveDestinations(location ? location : "");
+            
             let response;
             // filter destinations to only give contain destinations based on users location
 
