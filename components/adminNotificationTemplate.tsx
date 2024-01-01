601aae45726476d3dba1c1292b5ef7c05d70b66b
sam <samuelironkwec@gmail.com>
2024-01-01 12:01:03 -0600

    bugs

--- components/adminNotificationTemplate.tsx
+++ components/adminNotificationTemplate.tsx
@@ -6,7 +6,7 @@ export default function AdminNotificationTemplate(tripDetails: Trip) {
     return (
         <div>
             <p>Origin: {tripDetails.origin}</p>
-            <p>Destination: {tripDetails.destination.address}</p>
+            <p>Destination: {tripDetails.destination?.address}</p>
             <p>Date: {tripDetails.date}</p>
         </div>
     )
