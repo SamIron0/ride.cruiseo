e7beb67de58e0c659d9162f18658e2cef3c89524
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-10 00:47:13 +0000

    bugs

--- app/api/get-price/route.ts
+++ app/api/get-price/route.ts
@@ -1,48 +1,48 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch";
-import { getAddressJson } from "./controller";
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
+import { getAddressJson } from './controller';
+import { NextApiRequest, NextApiResponse } from 'next';
+import fetch from 'node-fetch';
+
+export default async function POST(req: Request) {
+  if (req.method === 'POST') {
     try {
-      const { origin, destination } = await req.body;
+      const { origin, destination } = await req.json();
       const [originJson, destinationJson] = await getAddressJson(
         origin,
         destination
       );
-      console.log("Received origin in api:", origin);
-      console.log("Received destination in api:", destination);
+      console.log('Received origin in api:', origin);
+      console.log('Received destination in api:', destination);
       //let destinationJson = await getAddressJson(destination);
       // Make a POST request to the Ngrok link
       const ngrokLink =
-        "https://500e-66-244-231-114.ngrok-free.app/execute-script";
+        'https://500e-66-244-231-114.ngrok-free.app/execute-script';
       const response = await fetch(ngrokLink, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ originJson, destinationJson }),
+        body: JSON.stringify({ originJson, destinationJson })
       });
-      console.log("Ngrok response:", response);
+      console.log('Ngrok response:', response);
       // Check if the request was successful (status code 2xx)
       if (response.ok) {
         const result = await response.json();
-        return res.status(200).json(result);
+        return new Response(JSON.stringify(response), {
+          status: 200
+        });
       } else {
-        // Handle other HTTP status codes
-        const error = await response.json();
-        return res.status(response.status).json(error);
+        return new Response(JSON.stringify({ error: { statusCode: 500 } }));
       }
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ error: { statusCode: 500, message: err.message } });
+    } catch (err: any) {
+      return new Response(
+        JSON.stringify({ error: { statusCode: 500, message: err.message } })
+      );
     }
   } else {
-    return res
-      .status(405)
-      .json({ error: { statusCode: 405, message: "Method Not Allowed" } });
+    return new Response(
+      JSON.stringify({
+        error: { statusCode: 405, message: 'Method Not Allowed' }
+      })
+    );
   }
 }
