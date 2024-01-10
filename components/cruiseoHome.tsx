d30b2f7b52fa780ea481ab535943f9a259878709
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-10 10:12:46 -0600

    x

--- components/cruiseoHome.tsx
+++ components/cruiseoHome.tsx
@@ -148,6 +148,7 @@ export default function CruiseoHome({
 
           </div>
           <div className='max-w-full'>
+            <AllTripsGrid onSelectDestination={handleDestinationSelect} userLocation={region} destinations={destinations} airportDestinations={airportDestinations} schoolDestinations={schoolDestinations} shopDestinations={shopDestinations} cinemaDestinations={cinemaDestinations} />
           </div>
           {!user ?
             <>
