2e5c2d4a8b38d1f1c13261cc8d3ac72f127de8c8
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-10 10:22:15 -0600

    x

--- components/allTripsGrid.tsx
+++ components/allTripsGrid.tsx
@@ -166,10 +166,7 @@ export function AllTripsGrid({
                 className="mt-2 cursor-pointer"
                 onClick={() => onSelectDestination(airport)}
               >
-                <DestinationCard
-                  destination={airport}
-                  price={"await getPrice(airport)"}
-                />
+                
               </div>
             ))}
           </div>
