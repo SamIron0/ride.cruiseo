c7a1ad87a6fc082aca8a1b9a59a3a793e91dbdd1
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-12 00:44:02 -0600

    w

--- components/priceProvider.tsx
+++ components/priceProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 
-const PriceContext = createContext();
+const PriceContext = createContext(null);
 
 export function PriceProvider({ children }) {
   const [price, setPrice] = useState('');
