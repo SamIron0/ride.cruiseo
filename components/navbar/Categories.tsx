99e85666c70e6457601ab886dc0ce884f6ecdd11
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-19 16:24:52 -0600

    x

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -70,7 +70,7 @@ const Categories = (onCategoryClick: any) => {
     <Container>
       <div
         className="
-        pt-4 flex px-10 sm:px-24 md:px-44 lg:px-60 flex-row items-center justify-between overflow-x-auto"
+        pt-4 flex px-10 sm:px-24 md:px-44 lg:px-64 xl:px-80 flex-row items-center justify-between overflow-x-auto"
       >
         {categories.map((item, index) => (
           <CategoryBox
