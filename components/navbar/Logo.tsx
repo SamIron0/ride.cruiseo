6a9f9c2f2e6accfd2ef22f88273172b8041cfeb8
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-19 18:01:16 +0000

    x

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,35 +1,18 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { CiUser } from 'react-icons/ci';
 
 const Logo = () => {
   const router = useRouter();
 
   return (
     <div
-      onClick={() => router.push("/")}
+      onClick={() => router.push('/')}
       className="hidden md:block cursor-pointer"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="27"
-        height="27"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        className="lucide lucide-car-front"
-      >
-        <path d="m21 8-2 2-1.5-3.7A2 2 0 0 0 15.646 5H8.4a2 2 0 0 0-1.903 1.257L5 10 3 8" />
-        <path d="M7 14h.01" />
-        <path d="M17 14h.01" />
-        <rect width="18" height="8" x="3" y="10" rx="2" />
-        <path d="M5 18v2" />
-        <path d="M19 18v2" />
-      </svg>
+      <span className="w-[100px]"> cruiseo</span> <CiUser />
     </div>
   );
 };
