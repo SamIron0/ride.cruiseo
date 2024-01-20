307e8ec4c37b631fb495ceafae0b89abe4226d37
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-19 19:10:08 -0600

    x

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -34,7 +34,7 @@ export default function Account() {
         data-drawer-toggle="separator-sidebar"
         aria-controls="separator-sidebar"
         type="button"
-        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
       >
         <span className="sr-only">Open sidebar</span>
         <svg
@@ -57,7 +57,7 @@ export default function Account() {
         id="separator-sidebar"
         className={`fixed top-0 left-0 z-40 w-64 h-full transition-transform ${
           isSidebarOpen ? "" : "-translate-x-full"
-        } sm:translate-x-0`}
+        }`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
