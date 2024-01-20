4ca9f626ba83bf8d184245ae9e5bfdbdda41358b
Samuel Ironkwe <127967090+SamIron0@users.noreply.github.com>
2024-01-19 18:47:15 -0600

    x

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,13 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSupabase } from "../supabase-provider";
-export default async function Account() {
+
+export default function Account() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
+
+  const closeSidebar = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", closeSidebar);
+
+    return () => {
+      document.removeEventListener("click", closeSidebar);
+    };
+  }, []);
+
   const { supabase } = useSupabase();
+
   return (
     <div className="pt-24">
       <button
@@ -35,8 +53,9 @@ export default async function Account() {
       </button>
 
       <aside
+        ref={sidebarRef}
         id="separator-sidebar"
-        className={`fixed left-0 z-40 w-64 h-full transition-transform ${
+        className={`fixed top-0 left-0 z-40 w-64 h-full transition-transform ${
           isSidebarOpen ? "" : "-translate-x-full"
         } sm:translate-x-0`}
         aria-label="Sidebar"
@@ -58,7 +77,7 @@ export default async function Account() {
                   <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
-                <span className="ms-3">Dashboard</span>
+                <span className="ms-3">Personal Details</span>
               </a>
             </li>
             <li>
@@ -75,7 +94,9 @@ export default async function Account() {
                 >
                   <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Kanban</span>
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Preferences
+                </span>
                 <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">
                   Pro
                 </span>
@@ -95,9 +116,8 @@ export default async function Account() {
                 >
                   <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  3
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Payment Settings
                 </span>
               </a>
             </li>
@@ -115,7 +135,7 @@ export default async function Account() {
                 >
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
+                <span className="flex-1 ms-3 whitespace-nowrap">Security</span>
               </a>
             </li>
             <li>
@@ -249,7 +269,7 @@ export default async function Account() {
               </a>
             </li>
           </ul>
-        </div>
+        </div>{" "}
       </aside>
 
       <div className="p-4 sm:ml-64">
@@ -507,7 +527,7 @@ export default async function Account() {
               </p>
             </div>
           </div>
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
