ea8b245f24788e92fe9079b3a4c3f154b3042ab9
sam <samuelironkwec@gmail.com>
2024-01-26 20:27:25 -0600

    x

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -151,73 +151,76 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
             locationValue={listing?.address}
             id={listing?.id}
           />
-          <div className="grid sm:grid-cols-2 grid-rows-2 sm:grid-rows-1 gap-4 ">
-            {listing.activeTrips?.map((trip: any) => (
-              <div className="w-full md:gap-6">
-                {/* Content */}
-                <div
-                  className="max-w-xl md:max-w-none md:w-full sm:mx-auto md:col-span-7 lg:col-span-6 "
-                  data-aos="fade-right"
-                >
-                  {/* Tabs buttons */}
-                  <div className="mb-6 text-white">
-                    <div
-                      className={`flex justify-between bg-zinc-800  w-full items-center text-lg p-3 rounded-lg  transition duration-300 ease-in-out hover:shadow-lg ${
-                        selectedTrip.id === trip.id
-                          ? `shadow-lg border-blue-500 border first-letter:hover:shadow-lg`
-                          : `  shadow-md border border-zinc-600 `
-                      }`}
-                    >
-                      <div className="flex flex-col">
-                        <div className="font-normal text-sm text-zinc-300 leading-snug tracking-tight mb-1">
-                          Price:{' '}
-                          <span className="text-white font-semibold">
-                            {loadedPrices?.get(trip.id)}
-                          </span>
-                        </div>
+          <div className="sm:flex sm:flex-1 gap-4 ">
+            <div className=" sm:pr-6 ">
+              {listing.activeTrips?.map((trip: any) => (
+                <div className="w-full md:gap-6">
+                  {/* Content */}
+                  <div
+                    className="max-w-xl md:max-w-none md:w-full sm:mx-auto md:col-span-7 lg:col-span-6 "
+                    data-aos="fade-right"
+                  >
+                    {/* Tabs buttons */}
+                    <div className=" text-white">
+                      <div
+                        className={`flex justify-between bg-zinc-800  w-full items-center text-lg p-3 rounded-lg  transition duration-300 ease-in-out hover:shadow-lg ${
+                          selectedTrip.id === trip.id
+                            ? `shadow-lg border-blue-500 border first-letter:hover:shadow-lg`
+                            : `  shadow-md border border-zinc-600 `
+                        }`}
+                      >
+                        <div className="flex flex-col">
+                          <div className="font-normal text-sm text-zinc-300 leading-snug tracking-tight mb-1">
+                            Price:{' '}
+                            <span className="text-white font-semibold">
+                              {loadedPrices?.get(trip.id)}
+                            </span>
+                          </div>
 
-                        <div className="font-normal text-zinc-300 text-smleading-snug tracking-tight mb-1">
-                          Time:{' '}
-                          <span className="text-white font-semibold">
-                            {trip.date}
-                          </span>
+                          <div className="font-normal text-zinc-300 text-smleading-snug tracking-tight mb-1">
+                            Time:{' '}
+                            <span className="text-white font-semibold">
+                              {trip.date}
+                            </span>
+                          </div>
+                          <div className="font-normal text-sm text-zinc-300 leading-snug tracking-tight mb-1">
+                            Riders:{' '}
+                            <span className="text-white font-semibold">
+                              {trip.user_ids.length}
+                            </span>
+                          </div>
                         </div>
-                        <div className="font-normal text-sm text-zinc-300 leading-snug tracking-tight mb-1">
-                          Riders:{' '}
-                          <span className="text-white font-semibold">
-                            {trip.user_ids.length}
-                          </span>
-                        </div>
-                      </div>
-                      <div className="flex gap-2 flex-col">
-                        <button
-                          onClick={() => getPrice(trip)}
-                          disabled={
-                            isLoading ||
-                            loadedPrices?.get(trip.id) !== undefined
-                          }
-                          className="flex text-sm pb-2 justify-center items-center px-4 py-2 bg-zinc-100 text-black rounded-lg shadow flex-shrink-0 ml-3 active:bg-zinc-300 transition duration-150 transform active:scale-110"
-                        >
-                          Show Price
-                        </button>
+                        <div className="flex gap-2 flex-col">
+                          <button
+                            onClick={() => getPrice(trip)}
+                            disabled={
+                              isLoading ||
+                              loadedPrices?.get(trip.id) !== undefined
+                            }
+                            className="flex text-sm pb-2 justify-center items-center px-4 py-2 bg-zinc-100 text-black rounded-lg shadow flex-shrink-0 ml-3 active:bg-zinc-300 transition duration-150 transform active:scale-110"
+                          >
+                            Show Price
+                          </button>
 
-                        <button
-                          disabled={isLoading || trip.id == selectedTrip.id}
-                          onClick={() => setSelectedTrip(trip)}
-                          className="flex text-sm justify-center items-center px-4 py-2 bg-fuchsia-600 text-white rounded-lg shadow flex-shrink-0 ml-3 active:bg-fuchsia-800 transition duration-150 transform active:scale-110"
-                        >
-                          Select
-                        </button>
-                      </div>
-                    </div>{' '}
+                          <button
+                            disabled={isLoading || trip.id == selectedTrip.id}
+                            onClick={() => setSelectedTrip(trip)}
+                            className="flex text-sm justify-center items-center px-4 py-2 bg-fuchsia-600 text-white rounded-lg shadow flex-shrink-0 ml-3 active:bg-fuchsia-800 transition duration-150 transform active:scale-110"
+                          >
+                            Select
+                          </button>
+                        </div>
+                      </div>{' '}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="h-[250px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100"></div>
+            <div className="w-full sm:pl-6 flex items-center justify-center">
+              <div className="h-12 w-12 bg-zinc-800">
 
-            <hr className="sm:hidden" />
-            <div className="w-full flex items-center justify-center">
-              x
+              </div>
             </div>
           </div>
 
