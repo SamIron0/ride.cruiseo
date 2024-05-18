c7befa9d676a69de59151a909e8839a200df262c
SamIron0 <samuelironkwec@gmail.com>
2024-05-17 20:46:18 -0500

    x

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,100 +1,38 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { differenceInDays } from 'date-fns';
 
 import useSearchModal from '@/app/hooks/useSearchModal';
+import { TextareaAutosize } from '../ui/textarea-autosize';
 
 const Search = () => {
   const searchModal = useSearchModal();
   const params = useSearchParams();
+  const [isTyping, setIsTyping] = useState<boolean>(false);
 
   const locationValue = params?.get('locationValue');
   const startDate = params?.get('startDate');
-  const endDate = params?.get('endDate');
-  const guestCount = params?.get('guestCount');
+  const locationLabel = 'Search';
+  const [input, setInput] = useState<string>('');
 
-  const locationLabel = 'Your Location';
-
-  const durationLabel = useMemo(() => {
-    if (startDate && endDate) {
-      const start = new Date(startDate as string);
-      const end = new Date(endDate as string);
-      let diff = differenceInDays(end, start);
-
-      if (diff === 0) {
-        diff = 1;
-      }
-
-      return `${diff} Days`;
-    }
-
-    return 'Any Day';
-  }, [startDate, endDate]);
-
+  const handleInputChange = (value: string) => {
+    setInput(value);
+  };
   return (
-    <div
-      onClick={searchModal.onOpen}
-      className="
-        border-[1px] 
-        border-[#232325]
-                w-full 
-        md:w-auto 
-        py-2 
-        rounded-full 
-        shadow-sm 
-        hover:shadow-md 
-        transition 
-        cursor-pointer
-      "
-    >
-      <div
-        className="
-          flex 
-          flex-row 
-          items-center 
-          justify-between
-        "
-      >
-        <div
-          className="
-            text-sm 
-            font-semibold 
-            px-6
-          text-zinc-300
-          "
-        >
-          {locationLabel}
-        </div>
-
-        <div
-          className="
-            text-sm 
-            pl-6 
-            pr-2 
-            text-zinc-400 
-            flex 
-            border-l-[1px]
-            flex-row 
-            items-center 
-            gap-3
-          "
-        >
-          <div className="hidden sm:block">{durationLabel}</div>
-          <div
-            className="
-              p-2 
-              bg-[#4169E1] 
-              rounded-full 
-              text-white
-            "
-          >
-            <BiSearch size={18} />
-          </div>
-        </div>
-      </div>
+    <div>
+      <TextareaAutosize
+        className="text-md flex w-full resize-none rounded-md border-none bg-transparent py-2 pl-3 pr-14 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+        placeholder={`Search`}
+        onValueChange={handleInputChange}
+        value={input}
+        minRows={1}
+        maxRows={18}
+        onCompositionStart={() => setIsTyping(true)}
+        onCompositionEnd={() => setIsTyping(false)}
+      />
     </div>
   );
 };
