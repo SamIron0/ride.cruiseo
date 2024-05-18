dc6e298c53283a9fbbc73fe63774a2552cfe4c89
SamIron0 <samuelironkwec@gmail.com>
2024-05-17 21:44:08 -0500

    x

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -7,6 +7,7 @@ import { differenceInDays } from 'date-fns';
 
 import useSearchModal from '@/app/hooks/useSearchModal';
 import { TextareaAutosize } from '../ui/textarea-autosize';
+import { cn } from '@/utils/helpers';
 
 const Search = () => {
   const searchModal = useSearchModal();
@@ -22,7 +23,7 @@ const Search = () => {
     setInput(value);
   };
   return (
-    <div>
+    <div className="relative mt-3 flex min-h-[60px] w-full items-center justify-center rounded-xl border-2 border-input">
       <TextareaAutosize
         className="text-md flex w-full resize-none rounded-md border-none bg-transparent py-2 pl-3 pr-14 ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
         placeholder={`Search`}
@@ -33,6 +34,16 @@ const Search = () => {
         onCompositionStart={() => setIsTyping(true)}
         onCompositionEnd={() => setIsTyping(false)}
       />
+      <div className="absolute bottom-[14px] right-3 flex cursor-pointer justify-center hover:opacity-50">
+        <BiSearch
+          className={cn(
+            'rounded bg-primary p-1 text-secondary',
+            !input ? 'cursor-not-allowed opacity-50' : ''
+          )}
+          onClick={() => {}}
+          size={30}
+        />
+      </div>
     </div>
   );
 };
