f9f57aef918d4429d75771a8405913b4e8de6eb4
SamIron0 <samuelironkwec@gmail.com>
2024-05-18 20:51:20 -0500

    x

--- app/destinations/layout.tsx
+++ app/destinations/layout.tsx
@@ -1,12 +1,15 @@
 'use client';
-
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from 'react';
+import { useSupabase } from '../supabase-provider';
+import { useRouter } from 'next/navigation';
 
 interface ListingsLayoutProps {
   children: ReactNode;
 }
 
 export default function ListingsLayout({ children }: ListingsLayoutProps) {
- 
+  const router = useRouter();
+  const { supabase } = useSupabase();
+
   return <>{children}</>;
 }
