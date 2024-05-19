638befa557ea7a85a57e43bd32b09b126a10ba6f
SamIron0 <samuelironkwec@gmail.com>
2024-05-18 20:12:32 -0500

    x

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { createServerSupabaseClient } from '@/app/supabase-server';
 import Categories from './Categories';
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
-import { UserDetails } from '@/types';
-import { User } from '@supabase/supabase-js';
-
 export default async function Navbar() {
  
   return (
