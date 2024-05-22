5c8b0d699036677a3b478909a6278762490e0e33
SamIron0 <samuelironkwec@gmail.com>
2024-05-21 19:47:22 -0500

    x

--- lib/supabase/browser-client.ts
+++ lib/supabase/browser-client.ts
@@ -1,4 +1,4 @@
-import { Database } from "@/supabase/types"
+import { Database } from "@/types_db"
 import { createBrowserClient } from "@supabase/ssr"
 
 export const supabase = createBrowserClient<Database>(
