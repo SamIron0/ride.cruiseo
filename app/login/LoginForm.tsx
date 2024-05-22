fb339b823cbe10f42a13ed3a028295cf41810487
SamIron0 <samuelironkwec@gmail.com>
2024-05-21 20:28:30 -0500

    x

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function LoginForm({ searchParams }: { searchParams: { message: string } }) {
   const router = useRouter();
