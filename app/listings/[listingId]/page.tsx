5420feee243b7f23a65c3ddb35dd4b5542094f7b
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 15:43:19 -0500

    x

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,30 +1,29 @@
-'use client';
+"use client"
 
-import ClientOnly from '@/components/ClientOnly';
-import EmptyState from '@/components/EmptyState';
-import ListingClient from './ListingClient';
-import { createServerSupabaseClient } from '@/app/supabase-server';
-import getListingById from '@/app/actions/getListingById';
+import ClientOnly from "@/components/ClientOnly"
+import EmptyState from "@/components/EmptyState"
+import ListingClient from "./ListingClient"
+import { getDestinationById } from "@/db/listings"
 
 interface IParams {
-  listingId?: string;
+  listingId?: string
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-  const listing = await getListingById(params.listingId);
+  const listing = await getDestinationById(params?.listingId as string)
   if (!listing) {
     return (
       <ClientOnly>
         <EmptyState />
       </ClientOnly>
-    );
+    )
   }
 
   return (
     <ClientOnly>
       <ListingClient listing={listing} />
     </ClientOnly>
-  );
-};
+  )
+}
 
-export default ListingPage;
+export default ListingPage
