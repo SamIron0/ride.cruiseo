d250b8eefb1ab67f16f0a07332bd8c19c8d671ac
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 18:38:06 -0500

    x

--- db/listings.ts
+++ db/listings.ts
@@ -1,48 +1,51 @@
 import { supabase } from "@/lib/supabase/browser-client"
-import { Destination, Trip } from "@/types";
+import { Destination, Trip } from "@/types"
 
 export const retrieveDestinations = async (): Promise<Destination[] | null> => {
   try {
     const { data: destinations } = await supabase
-      .from('destinations')
-      .select('id,name,address,photo,category');
-      
+      .from("destinations")
+      .select("*")
 
-    return destinations;
+    if (!destinations) {
+      return null
+    }
+
+    return destinations
   } catch (error) {
-    console.error('Error retrieving destinations:', error);
-    return null;
+    console.error("Error retrieving destinations:", error)
+    return null
   }
-};
+}
 
 export const getDestinationById = async (id: string) => {
   const destination = (
-    await supabase.from('destinations').select('*').eq('id', id).single()
-  ).data;
+    await supabase.from("destinations").select("*").eq("id", id).single()
+  ).data
   if (destination) {
     try {
       destination.activeTrips = (
         await Promise.all(
           destination.trip_ids.map(
-            async (tripId:any) =>
+            async (tripId: any) =>
               (
                 await supabase
-                  .from('trips')
-                  .select('*')
-                  .eq('id', tripId)
+                  .from("trips")
+                  .select("*")
+                  .eq("id", tripId)
                   .single()
               ).data
           )
         )
-      ).filter((trip) => trip?.status === 'Active') as Trip[];
+      ).filter(trip => trip?.status === "Active") as Trip[]
     } catch (error) {
-      console.error('Error retrieving destination:', error);
+      console.error("Error retrieving destination:", error)
     }
   }
-  return destination;
-};
+  return destination
+}
 //getDestinationById
 export const retrieveTimes = async (destinations: Destination[]) => {
-  const { data: times } = await supabase.from('destinations').select('');
-  return times;
-};
+  const { data: times } = await supabase.from("destinations").select("")
+  return times
+}
