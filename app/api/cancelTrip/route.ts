bee41ebf6b053cf804284a73c527d3b2126022ab
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 14:34:41 -0500

    x

---
+++ app/api/cancelTrip/route.ts
@@ -0,0 +1,45 @@
+import { createRouteHandlerClient, createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Trip } from "@/types";
+import { Database } from "@/types_db";
+import { cookies } from "next/headers";
+import { deleteTrip } from "@/utils/supabase-admin";
+
+export async function POST(req: Request,) {
+    if (req.method === 'POST') {
+
+        try {
+            const trip: Trip = await req.json();
+
+            const supabase = createRouteHandlerClient<Database>({ cookies });
+            const {
+                data: { session }
+            } = await supabase.auth.getSession();
+            //const session2 = await getSession()
+
+            if (!session) {
+                return new Response(JSON.stringify({
+                    error: 'not authenticated',
+                    description: 'The user does not have an active session or is not authenticated'
+                }), { status: 500 });
+            }
+
+            if (trip.id != null) {
+                const tripID = await deleteTrip(trip.id, session.user.id);
+                if (tripID != undefined) {
+                    const response = "Trip deleted";
+                    return new Response(JSON.stringify(response), {
+                        status: 200
+                    });
+                }
+                return new Response(JSON.stringify("Trip Deleted"), {
+                    status: 200
+                });
+            }
+        } catch (err: any) {
+            return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
+        }
+    } else {
+        return new Response(JSON.stringify('Method Not Allowed'));
+
+    }
+}
---
+++ app/api/getDestinations/route.ts
@@ -0,0 +1,28 @@
+import { createRouteHandlerClient, createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Trip, GeoCoordinate, Destination } from "@/types";
+import { retrieveDestinations } from "@/utils/supabase-admin";
+import { filterDestinations } from "./controller"
+
+export async function POST(req: Request) {
+    if (req.method === 'POST') {
+        try {
+            const region = await req.json();
+            const destinations: Destination[] | null = await retrieveDestinations();
+            let response;
+            // filter destinations to only give contain destinations based on users location
+            if (destinations != null) {
+                // change this to order by distance
+                //response = await filterDestinations(region, destinations)
+                return new Response(JSON.stringify(destinations), {
+                    status: 200
+                });
+            }
+
+        } catch (err: any) {
+            return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
+        }
+    } else {
+        return new Response(JSON.stringify('Method Not Allowed'));
+
+    }
+}
