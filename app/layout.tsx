aa91a98e2ffc7a826284df0477c50d84c2ed3493
SamIron0 <samuelironkwec@gmail.com>
2024-05-25 22:48:00 -0500

    z

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ import ToasterProvider from './providers/ToasterProvider';
 import ClientOnly from '@/components/ClientOnly';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+import { Database } from '@/types_db';
 let  favicon = '/favicon.svg';
 const meta = {
   title: 'Cruiseo Ride Share',
@@ -39,11 +42,25 @@ export const metadata = {
     description: meta.description
   }
 };
-export default function RootLayout({
+export default async function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children
 }: PropsWithChildren) {
+  const cookieStore = cookies()
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        }
+      }
+    }
+  )
+  const session = (await supabase.auth.getSession()).data.session
+
   return (
     <html lang="en">
       <head>
