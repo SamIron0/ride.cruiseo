04061303c6da19a4991c13073e5d8a81bf20a769
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 22:55:03 -0500

    x

--- app/[locale]/destinations/[destinationId]/ListingClient.tsx
+++ app/[locale]/destinations/[destinationId]/ListingClient.tsx
@@ -144,7 +144,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
           <button
             onClick={() => window.history.back()}
             type="button"
-            className="w-32 flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-all duration-200 bg-white border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700 hover:scale-105 active:scale-90"
+            className="w-32 flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-all duration-200 bg-white border rounded-lg gap-x-2 dark:hover:bg-gray-800 dark:bg-gray-900 dark:text-gray-200 dark:border-input hover:scale-105 active:scale-90"
           >
             <svg
               className="w-5 h-5 rtl:rotate-180"
