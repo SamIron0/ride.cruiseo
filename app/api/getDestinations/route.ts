8ec1e1ff06571b43e4a193d9382f6fc0d44dca43
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 19:27:46 -0500

    x

--- app/api/getDestinations/route.ts
+++ app/api/getDestinations/route.ts
@@ -1,25 +1,22 @@
-import {  Destination } from "@/types";
-import { retrieveDestinations } from "@/db/listings";
+import { Destination } from "@/types"
+import { retrieveDestinations } from "@/db/listings"
 
 export async function GET(req: Request) {
-    if (req.method === 'GET') {
-        try {
-            const destinations: Destination[] | null = await retrieveDestinations();
-            let response;
-            // filter destinations to only give contain destinations based on users location
-            if (destinations != null) {
-                // change this to order by distance
-                //response = await filterDestinations(region, destinations)
-                return new Response(JSON.stringify(destinations), {
-                    status: 200
-                });
-            }
-
-        } catch (err: any) {
-            return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
-        }
-    } else {
-        return new Response(JSON.stringify('Method Not Allowed'));
-
+  if (req.method === "GET") {
+    try {
+      const destinations: Destination[] | null = await retrieveDestinations()
+      let response
+      if (destinations != null) {
+        return new Response(JSON.stringify(destinations), {
+          status: 200
+        })
+      }
+    } catch (err: any) {
+      return new Response(
+        JSON.stringify({ error: { statusCode: 500, message: err.message } })
+      )
     }
+  } else {
+    return new Response(JSON.stringify("Method Not Allowed"))
+  }
 }
