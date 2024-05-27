8833d5d9f07f291a2a9a02761260089cb5c09017
SamIron0 <samuelironkwec@gmail.com>
2024-05-27 15:57:45 -0500

    x

--- components/ui/carpool-form/index.tsx
+++ components/ui/carpool-form/index.tsx
@@ -181,154 +181,146 @@ export const CarpoolForm = ({}: CarpoolFormProps) => {
     }
   }, [])
   return (
-    <div className="flex flex-col text-white items-center 2xl:px-22  h-screen w-full ">
-      <div className="w-full flex p-6 flex-col items-center justify-center">
-        <div className="w-full sm:px-28 md:px-44 lg:px-72 xl:px-96  3xl:[450px] pt-2 h-full">
-          <form
-            onSubmit={handleTripDetailsSubmit}
-            className=" h-fit flex flex-col items-center px-1 justify-center  w-full"
-          >
-            {destinationIsOpen ? (
-              <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
-                <div className="w-full">
-                  <h1 className=" font-semibold   text-lg ">Where to?</h1>
-                  <div className="relative overflow-hidden w-full h-40">
-                    <div className="absolute inset-y-0 right-0 z-10 bg-black w-1/12"></div>
-                  </div>
-                  {!destinationIsValid && (
-                    <div className="text-red-500 text-left  text-xs">
-                      Destination cannot be blank
-                    </div>
-                  )}
-                </div>
-              </div>
-            ) : (
-              <div
-                onClick={() => {
-                  setDateIsOpen(false)
-                  setOriginIsOpen(false)
-                  setDestinationIsOpen(true)
-                }}
-                className="flex mb-4 flex-col items-center border-input border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
-              >
-                <div className="w-full flex flex-col justify-center">
-                  <span>
-                    <h1 className=" font-bold   text-lg">Where to?</h1>
-                  </span>
-                </div>
+    <form
+      onSubmit={handleTripDetailsSubmit}
+      className="flex flex-col text-white items-center max-w-xl justify-center w-full"
+    >
+      {destinationIsOpen ? (
+        <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
+          <div className="w-full">
+            <h1 className=" font-semibold   text-lg ">Where to?</h1>
+            <div className="relative overflow-hidden w-full h-40">
+              <div className="absolute inset-y-0 right-0 z-10 bg-black w-1/12"></div>
+            </div>
+            {!destinationIsValid && (
+              <div className="text-red-500 text-left  text-xs">
+                Destination cannot be blank
               </div>
             )}
-            {originIsOpen ? (
-              <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
-                <div className="w-full">
-                  <h1 className=" font-semibold   text-lg ">From where?</h1>
-                  <div className="mt-2  relative">
-                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          </div>
+        </div>
+      ) : (
+        <div
+          onClick={() => {
+            setDateIsOpen(false)
+            setOriginIsOpen(false)
+            setDestinationIsOpen(true)
+          }}
+          className="flex mb-4 flex-col items-center border-input border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
+        >
+          <div className="w-full flex flex-col justify-center">
+            <span>
+              <h1 className=" font-bold   text-lg">Where to?</h1>
+            </span>
+          </div>
+        </div>
+      )}
+      {originIsOpen ? (
+        <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
+          <div className="w-full">
+            <h1 className=" font-semibold   text-lg ">From where?</h1>
+            <div className="mt-2  relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                value={origin}
+                onChange={e => setOriginAndSuggestions(e.target.value)}
+                placeholder={"Search"}
+                className="bg-transparent p-4 placeholder:text-gray-400 text-gray-900 ring-0  rounded-lg ps-10 border focus:ring-black focus:border-black bg-gray-100 border-input outline-none w-full "
+              ></input>
+            </div>
+
+            {!originIsValid && (
+              <div className="text-red-500 text-left  text-xs">
+                Origin cannot be blank
+              </div>
+            )}
+            {originSuggestionIsOpen && (
+              <div
+                ref={originRef}
+                className={
+                  formattedOriginOptions.length > 0
+                    ? "w-5/6 sm:w-3/5 md:w-3/6 lg:w-3/7 xl:w-2/5 z-10 p-2 w-50 absolute mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 left-1/2 transform -translate-x-1/2"
+                    : ""
+                }
+              >
+                {formattedOriginOptions?.map((formatOption, index) => (
+                  <button
+                    onClick={() => onOriginSuggestionClick(formatOption.value)}
+                    className="text-md hover:bg-gray-100 flex items-center text-left w-full p-1"
+                    key={index}
+                  >
+                    <div className="bg-gray-100 flex rounded-xl justify-center items-center p-2 mr-3">
+                      {" "}
                       <svg
-                        className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                        aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 20"
+                        height="26"
+                        viewBox="0 -960 960 960"
+                        width="26"
                       >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                        />
+                        <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
                       </svg>
                     </div>
-                    <input
-                      value={origin}
-                      onChange={e => setOriginAndSuggestions(e.target.value)}
-                      placeholder={"Search"}
-                      className="bg-transparent p-4 placeholder:text-gray-400 text-gray-900 ring-0  rounded-lg ps-10 border focus:ring-black focus:border-black bg-gray-100 border-input outline-none w-full "
-                    ></input>
-                  </div>
-
-                  {!originIsValid && (
-                    <div className="text-red-500 text-left  text-xs">
-                      Origin cannot be blank
-                    </div>
-                  )}
-                  {originSuggestionIsOpen && (
-                    <div
-                      ref={originRef}
-                      className={
-                        formattedOriginOptions.length > 0
-                          ? "w-5/6 sm:w-3/5 md:w-3/6 lg:w-3/7 xl:w-2/5 z-10 p-2 w-50 absolute mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 left-1/2 transform -translate-x-1/2"
-                          : ""
-                      }
-                    >
-                      {formattedOriginOptions?.map((formatOption, index) => (
-                        <button
-                          onClick={() =>
-                            onOriginSuggestionClick(formatOption.value)
-                          }
-                          className="text-md hover:bg-gray-100 flex items-center text-left w-full p-1"
-                          key={index}
-                        >
-                          <div className="bg-gray-100 flex rounded-xl justify-center items-center p-2 mr-3">
-                            {" "}
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              height="26"
-                              viewBox="0 -960 960 960"
-                              width="26"
-                            >
-                              <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
-                            </svg>
-                          </div>
-                          {formatOption.value}
-                        </button>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              </div>
-            ) : (
-              <div
-                onClick={() => {
-                  setDateIsOpen(false)
-                  setOriginIsOpen(true)
-                  setDestinationIsOpen(false)
-                }}
-                className="flex mb-4 flex-col items-center border-gray-300 border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
-              >
-                <div className="w-full flex flex-col justify-center">
-                  <span>
-                    <h1 className=" font-bold   text-lg">From where?</h1>
-                  </span>
-                </div>
+                    {formatOption.value}
+                  </button>
+                ))}
               </div>
             )}
-            {dateIsOpen ? (
-              <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
-                <div className="w-full flex flex-col justify-center">
-                  <h1 className=" font-semibold   text-lg">When?</h1>
-                </div>
-              </div>
-            ) : (
-              <div
-                onClick={() => {
-                  setDateIsOpen(true)
-                  setOriginIsOpen(false)
-                  setDestinationIsOpen(false)
-                }}
-                className="flex mb-4 flex-col items-center border-input border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
-              >
-                <div className="w-full flex flex-col justify-center">
-                  <span>
-                    <h1 className=" font-bold   text-lg ">When?</h1>
-                  </span>
-                </div>
-              </div>
-            )}
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div
+          onClick={() => {
+            setDateIsOpen(false)
+            setOriginIsOpen(true)
+            setDestinationIsOpen(false)
+          }}
+          className="flex mb-4 flex-col items-center border-gray-300 border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
+        >
+          <div className="w-full flex flex-col justify-center">
+            <span>
+              <h1 className=" font-bold   text-lg">From where?</h1>
+            </span>
+          </div>
+        </div>
+      )}
+      {dateIsOpen ? (
+        <div className="flex flex-col mb-4 border-input  border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40">
+          <div className="w-full flex flex-col justify-center">
+            <h1 className=" font-semibold   text-lg">When?</h1>
+          </div>
+        </div>
+      ) : (
+        <div
+          onClick={() => {
+            setDateIsOpen(true)
+            setOriginIsOpen(false)
+            setDestinationIsOpen(false)
+          }}
+          className="flex mb-4 flex-col items-center border-input border w-full p-6 lg:p-12 h-lg shadow-lg rounded-3xl shadow-blue-gray-500/40"
+        >
+          <div className="w-full flex flex-col justify-center">
+            <span>
+              <h1 className=" font-bold   text-lg ">When?</h1>
+            </span>
+          </div>
+        </div>
+      )}
+    </form>
   )
 }
