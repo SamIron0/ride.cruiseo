e6ed375f5f9fc04dac1226594e2221ecf3f5d6d8
SamIron0 <samuelironkwec@gmail.com>
2024-05-26 19:49:20 -0500

    x

--- db/admin.ts
+++ db/admin.ts
@@ -3,6 +3,7 @@ import { stripe } from "@/utils/stripe/config"
 import { createClient } from "@supabase/supabase-js"
 import Stripe from "stripe"
 import type { Database, Tables, TablesInsert } from "@/supabase/types"
+import { Trip } from "@/types"
 
 type Product = Tables<"products">
 type Price = Tables<"prices">
@@ -17,6 +18,45 @@ const supabaseAdmin = createClient<Database>(
   process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 )
 
+export const retrieveDestinations = async () => {
+  const { data: destinations, error } = await supabaseAdmin
+    .from("destinations")
+    .select("id,name,address,photo,category")
+
+  if (error) {
+    console.error("Error retrieving destinations:", error)
+    return null
+  }
+
+  return destinations
+}
+
+export const getDestinationById = async (id: string) => {
+  const destination = (
+    await supabaseAdmin.from("destinations").select("*").eq("id", id).single()
+  ).data
+  if (destination) {
+    try {
+      destination.activeTrips = (
+        await Promise.all(
+          destination.trip_ids.map(
+            async (tripId: any) =>
+              (
+                await supabaseAdmin
+                  .from("trips")
+                  .select("*")
+                  .eq("id", tripId)
+                  .single()
+              ).data
+          )
+        )
+      ).filter(trip => trip?.status === "Active") as Trip[]
+    } catch (error) {
+      console.error("Error retrieving destination:", error)
+    }
+  }
+  return destination
+}
 const upsertProductRecord = async (product: Stripe.Product) => {
   const productData: Product = {
     id: product.id,
--- db/listings.ts
+++ db/listings.ts
@@ -5,45 +5,7 @@ import { Destination, Trip } from "@/types"
 import { cookies } from "next/headers"
 
 const supabase = createClient(cookies())
-export const retrieveDestinations = async () => {
-  const { data: destinations, error } = await supabase
-    .from("destinations")
-    .select("id,name,address,photo,category")
 
-  if (error) {
-    console.error("Error retrieving destinations:", error)
-    return null
-  }
-
-  return destinations
-}
-
-export const getDestinationById = async (id: string) => {
-  const destination = (
-    await supabase.from("destinations").select("*").eq("id", id).single()
-  ).data
-  if (destination) {
-    try {
-      destination.activeTrips = (
-        await Promise.all(
-          destination.trip_ids.map(
-            async (tripId: any) =>
-              (
-                await supabase
-                  .from("trips")
-                  .select("*")
-                  .eq("id", tripId)
-                  .single()
-              ).data
-          )
-        )
-      ).filter(trip => trip?.status === "Active") as Trip[]
-    } catch (error) {
-      console.error("Error retrieving destination:", error)
-    }
-  }
-  return destination
-}
 //getDestinationById
 export const retrieveTimes = async (destinations: Destination[]) => {
   const { data: times } = await supabase.from("destinations").select("")
