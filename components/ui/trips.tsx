1d59342eed99f727d0cb5e0201e29049d3bbac89
SamIron0 <samuelironkwec@gmail.com>
2024-05-28 16:33:40 -0500

    x

--- components/ui/trips.tsx
+++ components/ui/trips.tsx
@@ -7,18 +7,23 @@ export const Trips = ({ trips }: TripsProps) => {
   return (
     <div className="p-4">
       {trips.map(trip => (
-        <div className="flex text-sm flex-row items-center border border-input mb-2 p-4 rounded">
-          <div key={trip.id} className="flex flex-col justify-between">
+        <div
+          key={trip.id}
+          className="flex text-sm flex-col items-center border border-input mb-2 p-4 rounded"
+        >
+          <div className="flex flex-row justify-between">
             <span>{trip.date}</span>
-            <span className="font-semibold">2 seats</span>
-            <span>${trip.price}</span>
+            <div className="flex flex-row">
+              <span className="font-semibold">2 seats</span>
+              <span>${trip.price}</span>
+            </div>
           </div>
           <span className="flex flex-col text-sm ">{trip.origin}</span>
           <span className="flex flex-col text-sm">{trip.origin}</span>
           <div className="flex -space-x-4 rtl:space-x-reverse">
             <img
               className="w-8 h-8 border-2  rounded-full border-gray-800"
-              src="/docs/images/people/profile-picture-5.jpg"
+              src="public/forks.jpg"
               alt=""
             />
             <img
