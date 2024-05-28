b36396c193bc7b81db62bc7621f2158adc361ef8
SamIron0 <samuelironkwec@gmail.com>
2024-05-28 16:22:09 -0500

    x

--- components/ui/trips.tsx
+++ components/ui/trips.tsx
@@ -7,9 +7,34 @@ export const Trips = ({ trips }: TripsProps) => {
   return (
     <div className="p-4">
       {trips.map(trip => (
-        <div>
-          <div key={trip.id}>{trip.date}</div>
-          <div>{trip.price}</div>
+        <div className="flex flex-row items-center border border-input mb-2 p-4 rounded">
+          <div key={trip.id} className="flex flex-col justify-between">
+            {trip.price}
+          </div>
+          <span className="flex flex-col text-sm ">{trip.origin}</span>
+          <span className="flex flex-col text-sm">{trip.origin}</span>
+          <div className="flex -space-x-4 rtl:space-x-reverse">
+            <img
+              className="w-8 h-8 border-2  rounded-full border-gray-800"
+              src="/docs/images/people/profile-picture-5.jpg"
+              alt=""
+            />
+            <img
+              className="w-8 h-8 border-2  rounded-full border-gray-800"
+              src="/docs/images/people/profile-picture-2.jpg"
+              alt=""
+            />
+            <img
+              className="w-8 h-8 border-2  rounded-full border-gray-800"
+              src="/docs/images/people/profile-picture-3.jpg"
+              alt=""
+            />
+            <img
+              className="w-8 h-8 border-2  rounded-full border-gray-800"
+              src="/docs/images/people/profile-picture-4.jpg"
+              alt=""
+            />
+          </div>{" "}
         </div>
       ))}
     </div>
