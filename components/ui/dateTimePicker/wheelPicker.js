b6dec50661a21a5561575de510c4fc8c7a8fd14a
SamIron0 <samuelironkwec@gmail.com>
2024-05-28 11:42:36 -0500

    x

--- components/ui/dateTimePicker/wheelPicker.js
+++ components/ui/dateTimePicker/wheelPicker.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useMemo, useRef } from "react"
+import React, { useEffect, useMemo, useRef } from "react";
 
-import "./styles.css"
+import "./styles.css";
 
 const WheelPickerComponent = ({
   dateItems,
@@ -18,15 +18,15 @@ const WheelPickerComponent = ({
   containerHeight = 210,
   itemHeight = 32
 }) => {
-  const hourItemsContRef = useRef()
-  const dateItemsContRef = useRef()
-  const minuteItemsContRef = useRef()
-  const ampmItemsContRef = useRef()
-  const isScrolling = useRef(false)
-  const dateRefs = useRef([])
-  const hourRefs = useRef([])
-  const minuteRefs = useRef([])
-  const ampmRefs = useRef([])
+  const hourItemsContRef = useRef();
+  const dateItemsContRef = useRef();
+  const minuteItemsContRef = useRef();
+  const ampmItemsContRef = useRef();
+  const isScrolling = useRef(false);
+  const dateRefs = useRef([]);
+  const hourRefs = useRef([]);
+  const minuteRefs = useRef([]);
+  const ampmRefs = useRef([]);
   const dateItemsMap = useMemo(
     () =>
       dateItems.reduce(
@@ -34,8 +34,8 @@ const WheelPickerComponent = ({
         new Map()
       ),
     [dateItems]
-  )
-  const currentDateValue = useRef(dateItemsMap.get(dateValue) ?? 0)
+  );
+  const currentDateValue = useRef(dateItemsMap.get(dateValue) ?? 0);
   const hourItemsMap = useMemo(
     () =>
       hourItems.reduce(
@@ -43,8 +43,8 @@ const WheelPickerComponent = ({
         new Map()
       ),
     [hourItems]
-  )
-  const currentHourValue = useRef(hourItemsMap.get(hourValue) ?? 0)
+  );
+  const currentHourValue = useRef(hourItemsMap.get(hourValue) ?? 0);
   const minuteItemsMap = useMemo(
     () =>
       minuteItems.reduce(
@@ -52,8 +52,8 @@ const WheelPickerComponent = ({
         new Map()
       ),
     [minuteItems]
-  )
-  const currentMinuteValue = useRef(minuteItemsMap.get(minuteValue) ?? 0)
+  );
+  const currentMinuteValue = useRef(minuteItemsMap.get(minuteValue) ?? 0);
   const ampmItemsMap = useMemo(
     () =>
       ampmItems.reduce(
@@ -61,12 +61,12 @@ const WheelPickerComponent = ({
         new Map()
       ),
     [ampmItems]
-  )
-  const currentAmpmValue = useRef(ampmItemsMap.get(ampmValue) ?? 0)
+  );
+  const currentAmpmValue = useRef(ampmItemsMap.get(ampmValue) ?? 0);
 
-  const visibleItemsCount = Math.floor(containerHeight / itemHeight)
-  const offset = Math.round((visibleItemsCount + 1) / 2) + 1
-  const maxScrollOffset = (containerHeight - itemHeight) / 2
+  const visibleItemsCount = Math.floor(containerHeight / itemHeight);
+  const offset = Math.round((visibleItemsCount + 1) / 2) + 1;
+  const maxScrollOffset = (containerHeight - itemHeight) / 2;
 
   function rerenderDateElements(
     selectedElement,
@@ -78,17 +78,17 @@ const WheelPickerComponent = ({
       dateRefs.current
         .slice(firstItemIndex, lastItemIndex)
         .forEach((item, index) => {
-          const realIndex = index + firstItemIndex
+          const realIndex = index + firstItemIndex;
           const scrollOffset = Math.min(
             Math.abs(scrollTop - realIndex * itemHeight - itemHeight / 2),
             maxScrollOffset
-          )
-          const sin = scrollOffset / maxScrollOffset
-          const cos = Math.sqrt(1 - sin ** 2)
-          const [div] = item.getElementsByTagName("div")
-          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`
-          div.style.transformOrigin = "right"
-        })
+          );
+          const sin = scrollOffset / maxScrollOffset;
+          const cos = Math.sqrt(1 - sin ** 2);
+          const [div] = item.getElementsByTagName("div");
+          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`;
+          div.style.transformOrigin = "right";
+        });
     }
   }
 
@@ -102,17 +102,17 @@ const WheelPickerComponent = ({
       hourRefs.current
         .slice(firstItemIndex, lastItemIndex)
         .forEach((item, index) => {
-          const realIndex = index + firstItemIndex
+          const realIndex = index + firstItemIndex;
           const scrollOffset = Math.min(
             Math.abs(scrollTop - realIndex * itemHeight - itemHeight / 2),
             maxScrollOffset
-          )
-          const sin = scrollOffset / maxScrollOffset
-          const cos = Math.sqrt(1 - sin ** 2)
-          const [div] = item.getElementsByTagName("div")
-          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`
-          div.style.transformOrigin = "center"
-        })
+          );
+          const sin = scrollOffset / maxScrollOffset;
+          const cos = Math.sqrt(1 - sin ** 2);
+          const [div] = item.getElementsByTagName("div");
+          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`;
+          div.style.transformOrigin = "center";
+        });
     }
   }
 
@@ -126,17 +126,17 @@ const WheelPickerComponent = ({
       minuteRefs.current
         .slice(firstItemIndex, lastItemIndex)
         .forEach((item, index) => {
-          const realIndex = index + firstItemIndex
+          const realIndex = index + firstItemIndex;
           const scrollOffset = Math.min(
             Math.abs(scrollTop - realIndex * itemHeight - itemHeight / 2),
             maxScrollOffset
-          )
-          const sin = scrollOffset / maxScrollOffset
-          const cos = Math.sqrt(1 - sin ** 2)
-          const [div] = item.getElementsByTagName("div")
-          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`
-          div.style.transformOrigin = "left"
-        })
+          );
+          const sin = scrollOffset / maxScrollOffset;
+          const cos = Math.sqrt(1 - sin ** 2);
+          const [div] = item.getElementsByTagName("div");
+          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`;
+          div.style.transformOrigin = "left";
+        });
     }
   }
 
@@ -150,265 +150,269 @@ const WheelPickerComponent = ({
       ampmRefs.current
         .slice(firstItemIndex, lastItemIndex)
         .forEach((item, index) => {
-          const realIndex = index + firstItemIndex
+          const realIndex = index + firstItemIndex;
           const scrollOffset = Math.min(
             Math.abs(scrollTop - realIndex * itemHeight - itemHeight / 2),
             maxScrollOffset
-          )
-          const sin = scrollOffset / maxScrollOffset
-          const cos = Math.sqrt(1 - sin ** 2)
-          const [div] = item.getElementsByTagName("div")
-          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`
-          div.style.transformOrigin = "left"
-        })
+          );
+          const sin = scrollOffset / maxScrollOffset;
+          const cos = Math.sqrt(1 - sin ** 2);
+          const [div] = item.getElementsByTagName("div");
+          div.style.transform = `rotateX(${Math.asin(sin)}rad) scale(${cos})`;
+          div.style.transformOrigin = "left";
+        });
     }
   }
 
   useEffect(() => {
-    let isAnimating = false
+    let isAnimating = false;
 
     function handleHourScroll(event) {
       if (!isAnimating) {
-        isAnimating = true
+        isAnimating = true;
 
         requestAnimationFrame(() => {
-          const scrollTop = Math.max(event.target.scrollTop, 0)
+          const scrollTop = Math.max(event.target.scrollTop, 0);
           const selectedElement = Math.min(
             Math.max(Math.floor(scrollTop / itemHeight), 0),
             hourItems.length - 1
-          )
-          window.clearTimeout(isScrolling.current)
-          rerenderHourElements(selectedElement, scrollTop)
+          );
+          window.clearTimeout(isScrolling.current);
+          rerenderHourElements(selectedElement, scrollTop);
 
-          currentHourValue.current = selectedElement
+          currentHourValue.current = selectedElement;
           isScrolling.current = setTimeout(function () {
-            handleHourChange(hourItems[selectedElement].value)
-          }, 20)
+            handleHourChange(hourItems[selectedElement].value);
+          }, 20);
 
-          isAnimating = false
-        })
+          isAnimating = false;
+        });
       }
     }
 
-    hourItemsContRef.current?.addEventListener("scroll", handleHourScroll)
+    hourItemsContRef.current?.addEventListener("scroll", handleHourScroll);
     hourRefs.current[currentDateValue.current]?.scrollIntoView({
       block: "center"
-    })
+    });
     rerenderHourElements(
       currentHourValue.current,
       hourItemsContRef.current?.scrollTop,
       0,
       hourItems.length
-    )
+    );
     return () => {
-      hourItemsContRef.current?.removeEventListener("scroll", handleHourScroll)
-    }
-  }, [hourItemsContRef.current])
+      hourItemsContRef.current?.removeEventListener("scroll", handleHourScroll);
+    };
+  }, [hourItemsContRef.current]);
 
   useEffect(() => {
-    let isAnimating = false
+    let isAnimating = false;
 
     function handleDateScroll(event) {
       if (!isAnimating) {
-        isAnimating = true
+        isAnimating = true;
 
         requestAnimationFrame(() => {
-          const scrollTop = Math.max(event.target.scrollTop, 0)
+          const scrollTop = Math.max(event.target.scrollTop, 0);
           const selectedElement = Math.min(
             Math.max(Math.floor(scrollTop / itemHeight), 0),
             dateItems.length - 1
-          )
-          window.clearTimeout(isScrolling.current)
-          rerenderDateElements(selectedElement, scrollTop)
+          );
+          window.clearTimeout(isScrolling.current);
+          rerenderDateElements(selectedElement, scrollTop);
 
-          currentDateValue.current = selectedElement
+          currentDateValue.current = selectedElement;
           isScrolling.current = setTimeout(function () {
-            handleDateChange(dateItems[selectedElement].value)
-          }, 20)
+            handleDateChange(dateItems[selectedElement].value);
+          }, 20);
 
-          isAnimating = false
-        })
+          isAnimating = false;
+        });
       }
     }
 
-    dateItemsContRef.current?.addEventListener("scroll", handleDateScroll)
+    dateItemsContRef.current?.addEventListener("scroll", handleDateScroll);
     dateRefs.current[currentDateValue.current]?.scrollIntoView({
       block: "center"
-    })
+    });
     rerenderDateElements(
       currentDateValue.current,
       dateItemsContRef.current?.scrollTop,
       0,
       dateItems.length
-    )
+    );
 
     return () => {
-      dateItemsContRef.current?.removeEventListener("scroll", handleDateScroll)
-    }
-  }, [dateItemsContRef.current])
+      dateItemsContRef.current?.removeEventListener("scroll", handleDateScroll);
+    };
+  }, [dateItemsContRef.current]);
 
   useEffect(() => {
-    let isAnimating = false
+    let isAnimating = false;
 
     function handleMinuteScroll(event) {
       if (!isAnimating) {
-        isAnimating = true
+        isAnimating = true;
 
         requestAnimationFrame(() => {
-          const scrollTop = Math.max(event.target.scrollTop, 0)
+          const scrollTop = Math.max(event.target.scrollTop, 0);
           const selectedElement = Math.min(
             Math.max(Math.floor(scrollTop / itemHeight), 0),
             minuteItems.length - 1
-          )
-          window.clearTimeout(isScrolling.current)
-          rerenderMinuteElements(selectedElement, scrollTop)
+          );
+          window.clearTimeout(isScrolling.current);
+          rerenderMinuteElements(selectedElement, scrollTop);
 
-          currentMinuteValue.current = selectedElement
+          currentMinuteValue.current = selectedElement;
           isScrolling.current = setTimeout(function () {
-            handleMinuteChange(minuteItems[selectedElement].value)
-          }, 20)
+            handleMinuteChange(minuteItems[selectedElement].value);
+          }, 20);
 
-          isAnimating = false
-        })
+          isAnimating = false;
+        });
       }
     }
 
-    minuteItemsContRef.current?.addEventListener("scroll", handleMinuteScroll)
+    minuteItemsContRef.current?.addEventListener("scroll", handleMinuteScroll);
     minuteRefs.current[currentDateValue.current]?.scrollIntoView({
       block: "center"
-    })
+    });
     rerenderMinuteElements(
       currentMinuteValue.current,
       minuteItemsContRef.current?.scrollTop,
       0,
       minuteRefs.length
-    )
+    );
     return () => {
       minuteItemsContRef.current?.removeEventListener(
         "scroll",
         handleMinuteScroll
-      )
-    }
-  }, [minuteItemsContRef.current])
+      );
+    };
+  }, [minuteItemsContRef.current]);
 
   useEffect(() => {
-    let isAnimating = false
+    let isAnimating = false;
 
     function handleAmpmScroll(event) {
       if (!isAnimating) {
-        isAnimating = true
+        isAnimating = true;
 
         requestAnimationFrame(() => {
-          const scrollTop = Math.max(event.target.scrollTop, 0)
+          const scrollTop = Math.max(event.target.scrollTop, 0);
           const selectedElement = Math.min(
             Math.max(Math.floor(scrollTop / itemHeight), 0),
             ampmItems.length - 1
-          )
-          window.clearTimeout(isScrolling.current)
-          rerenderAmpmElements(selectedElement, scrollTop)
+          );
+          window.clearTimeout(isScrolling.current);
+          rerenderAmpmElements(selectedElement, scrollTop);
 
-          currentAmpmValue.current = selectedElement
+          currentAmpmValue.current = selectedElement;
           isScrolling.current = setTimeout(function () {
-            handleAmpmChange(ampmItems[selectedElement].value)
-          }, 20)
+            handleAmpmChange(ampmItems[selectedElement].value);
+          }, 20);
 
-          isAnimating = false
-        })
+          isAnimating = false;
+        });
       }
     }
 
-    ampmItemsContRef.current?.addEventListener("scroll", handleAmpmScroll)
+    ampmItemsContRef.current?.addEventListener("scroll", handleAmpmScroll);
     ampmRefs.current[currentDateValue.current]?.scrollIntoView({
       block: "center"
-    })
+    });
     rerenderAmpmElements(
       currentAmpmValue.current,
       ampmItemsContRef.current?.scrollTop,
       0,
       ampmRefs.length
-    )
+    );
     return () => {
-      ampmItemsContRef.current?.removeEventListener("scroll", handleAmpmScroll)
-    }
-  }, [ampmItemsContRef.current])
+      ampmItemsContRef.current?.removeEventListener("scroll", handleAmpmScroll);
+    };
+  }, [ampmItemsContRef.current]);
 
   useEffect(() => {
-    const index = dateItemsMap.get(dateValue)
+    const index = dateItemsMap.get(dateValue);
     if (index !== currentDateValue.current) {
-      currentDateValue.current = index
+      currentDateValue.current = index;
       dateRefs.current[index]?.scrollIntoView({
         block: "center",
         behavior: "smooth"
-      })
+      });
       rerenderDateElements(
         currentDateValue.current,
         dateItemsContRef.current?.scrollTop,
         0,
         dateItems.length
-      )
+      );
     }
-  }, [dateValue])
+  }, [dateValue]);
 
   useEffect(() => {
-    const index = hourItemsMap.get(hourValue)
+    const index = hourItemsMap.get(hourValue);
     if (index !== currentHourValue.current) {
-      currentHourValue.current = index
+      currentHourValue.current = index;
       hourRefs.current[index]?.scrollIntoView({
         block: "center",
         behavior: "smooth"
-      })
+      });
       rerenderDateElements(
         currentHourValue.current,
         hourItemsContRef.current?.scrollTop,
         0,
         hourItems.length
-      )
+      );
     }
-  }, [hourValue])
+  }, [hourValue]);
 
   useEffect(() => {
-    const index = minuteItemsMap.get(minuteValue)
+    const index = minuteItemsMap.get(minuteValue);
     if (index !== currentMinuteValue.current) {
-      currentMinuteValue.current = index
+      currentMinuteValue.current = index;
       minuteRefs.current[index]?.scrollIntoView({
         block: "center",
         behavior: "smooth"
-      })
+      });
       rerenderDateElements(
         currentMinuteValue.current,
         minuteItemsContRef.current?.scrollTop,
         0,
         minuteItems.length
-      )
+      );
     }
-  }, [minuteValue])
+  }, [minuteValue]);
 
   useEffect(() => {
-    const index = ampmItemsMap.get(ampmValue)
+    const index = ampmItemsMap.get(ampmValue);
     if (index !== currentAmpmValue.current) {
-      currentAmpmValue.current = index
+      currentAmpmValue.current = index;
       ampmRefs.current[index]?.scrollIntoView({
         block: "center",
         behavior: "smooth"
-      })
+      });
       rerenderDateElements(
         currentAmpmValue.current,
         ampmItemsContRef.current?.scrollTop,
         0,
         ampmItems.length
-      )
+      );
     }
-  }, [ampmValue])
+  }, [ampmValue]);
 
   return (
-    <div className="container ">
-
+    <div
+      className="container"
+      style={{
+        height: `${containerHeight}px`
+      }}
+    >
       <ul className="items" ref={dateItemsContRef}>
         {dateItems.map((item, index) => (
           <li
             className="item"
             key={item.value}
-            ref={node => (dateRefs.current[index] = node)}
+            ref={(node) => (dateRefs.current[index] = node)}
             style={{
               height: `${itemHeight}px`,
               lineHeight: `${itemHeight}px`
@@ -423,7 +427,7 @@ const WheelPickerComponent = ({
           <li
             className="item"
             key={item.value}
-            ref={node => (hourRefs.current[index] = node)}
+            ref={(node) => (hourRefs.current[index] = node)}
             style={{
               height: `${itemHeight}px`,
               lineHeight: `${itemHeight}px`
@@ -438,7 +442,7 @@ const WheelPickerComponent = ({
           <li
             className="item"
             key={item.value}
-            ref={node => (minuteRefs.current[index] = node)}
+            ref={(node) => (minuteRefs.current[index] = node)}
             style={{
               height: `${itemHeight}px`,
               lineHeight: `${itemHeight}px`
@@ -453,7 +457,7 @@ const WheelPickerComponent = ({
           <li
             className="item"
             key={item.value}
-            ref={node => (ampmRefs.current[index] = node)}
+            ref={(node) => (ampmRefs.current[index] = node)}
             style={{
               height: `${itemHeight}px`,
               lineHeight: `${itemHeight}px`
@@ -464,7 +468,7 @@ const WheelPickerComponent = ({
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export const WheelPicker = React.memo(WheelPickerComponent)
+export const WheelPicker = React.memo(WheelPickerComponent);
