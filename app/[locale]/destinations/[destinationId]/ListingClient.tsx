20a0a0751a729ca7bdb023c3ce394a752ad55cc9
SamIron0 <samuelironkwec@gmail.com>
2024-05-28 16:42:10 -0500

    x

--- app/[locale]/destinations/[destinationId]/ListingClient.tsx
+++ app/[locale]/destinations/[destinationId]/ListingClient.tsx
@@ -52,7 +52,7 @@ const ListingClient: React.FC<ListingClientProps> = ({ listing }) => {
         price: 25,
         date: dateTime.date,
         origin: origin,
-        destination: listing.addresss
+        destination: listing.address
       }
     ])
   }, [dateTime.date, origin])
