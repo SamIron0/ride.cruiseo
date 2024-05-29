e86a47c33c0d0c16395b8515bd3cf3ae230b77f9
SamIron0 <samuelironkwec@gmail.com>
2024-05-29 15:23:11 -0500

    x

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,7 +1,41 @@
 "use client"
-import { Grid } from "@/components/Grid"
-import Navbar from "@/components/navbar/NavBar"
-import { createClient } from "@/lib/supabase/server"
+import { useRouter } from "next/navigation"
+import React, { useEffect, useState } from "react"
+
 export default function Dashboard() {
-  return <>Home </>
+  const [status, setStatus] = useState(null)
+  const [customerEmail, setCustomerEmail] = useState("")
+  const router = useRouter()
+  useEffect(() => {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const sessionId = urlParams.get("session_id")
+
+    fetch(`/api/checkout_sessions?session_id=${sessionId}`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(data => {
+        setStatus(data.status)
+        setCustomerEmail(data.customer_email)
+      })
+  }, [])
+
+  if (status === "open") {
+    return router.push("/")
+  }
+
+  if (status === "complete") {
+    return (
+      <section id="success">
+        <p>
+          We appreciate your business! A confirmation email will be sent to{" "}
+          {customerEmail}. If you have any questions, please email{" "}
+          <a href="mailto:orders@example.com">orders@example.com</a>.
+        </p>
+      </section>
+    )
+  }
+
+  return null
 }
