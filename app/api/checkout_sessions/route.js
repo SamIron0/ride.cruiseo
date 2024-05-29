fe129f9f99ff36a71e3d3bd53f4e8e0724504582
SamIron0 <samuelironkwec@gmail.com>
2024-05-29 13:42:33 -0500

    x

--- app/api/checkout_sessions/route.js
+++ app/api/checkout_sessions/route.js
@@ -21,7 +21,7 @@ export async function POST(req) {
       ui_mode: 'embedded',
       line_items: [
         {
-          price: priceId,
+          price: price.id,
           quantity: 1,
         },
       ],
