b5be8c8aaed1c7f3a308922bea7442ce51029ff0
SamIron0 <samuelironkwec@gmail.com>
2024-05-31 11:14:05 -0500

    x

--- app/[locale]/destinations/[destinationId]/page.tsx
+++ app/[locale]/destinations/[destinationId]/page.tsx
@@ -3,6 +3,7 @@
 import ClientOnly from "@/components/ClientOnly"
 import ListingClient from "./ListingClient"
 import { useEffect, useState } from "react"
+import Container from "@/components/Container"
 
 interface IParams {
   destinationId?: string
@@ -35,18 +36,30 @@ const ListingPage = ({ params }: { params: IParams }) => {
   if (!destination) {
     return (
       <ClientOnly>
-        <div role="status" className="max-w-3xl animate-pulse">
-          <div className="h-5 rounded-full bg-gray-700 w-4 mb-6"></div>
-          <div className="h-2.5 rounded-full bg-gray-700 w-8 mb-4"></div>
-          <div className="h-12 rounded-full bg-gray-700 w-48 mb-8"></div>
-        </div>
+        <Container>
+          <div
+            className="
+          max-w-3xl 
+          mx-auto
+          pt-6
+        "
+          >
+            <div className="flex flex-col pb-12  gap-6">
+              <div className="w-32 flex  px-5 py-2 text-sm text-gray-700 transition-all duration-200 border rounded-lg gap-x-2 dark:hover:bg-zinc-800 dark:bg-background dark:text-zinc-200 dark:border-input hover:scale-105 active:scale-90"></div>
+              <div className=" w-32 h-[32px] rounded-lg bg-background "></div>
+              <div className=" w-64 h-[32px] rounded-lg bg-background "></div>
+              <div className=" w-full max-w-3xl h-[147.95px] rounded-lg bg-background "></div>
+              <div className=" w-full max-w-3xl h-[130px] rounded-lg bg-background "></div>
+            </div>
+          </div>
+        </Container>
       </ClientOnly>
     )
   }
 
   return (
     <ClientOnly>
-      <ListingClient listing={destination}  />
+      <ListingClient listing={destination} />
     </ClientOnly>
   )
 }
