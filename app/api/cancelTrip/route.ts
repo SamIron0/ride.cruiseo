62b470fa21068d84e4feb33889bca099ae2c4b9a
SamIron0 <samuelironkwec@gmail.com>
2024-06-04 00:27:56 -0500

    d

--- app/api/cancelTrip/route.ts
+++ app/api/cancelTrip/route.ts
@@ -1,45 +1,49 @@
-import { createRouteHandlerClient, createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { Trip } from "@/types";
-import { cookies } from "next/headers";
-import { cancelTrip } from "@/db/admin";
-import { Database } from "@/supabase/types";
+import {
+  createRouteHandlerClient,
+  createServerSupabaseClient
+} from "@supabase/auth-helpers-nextjs"
+import { Trip } from "@/types"
+import { cookies } from "next/headers"
+import { cancelTrip } from "@/db/admin"
+import { Database } from "@/supabase/types"
 
-export async function POST(req: Request,) {
-    if (req.method === 'POST') {
+export async function POST(req: Request) {
+  if (req.method === "POST") {
+    try {
+      const trip: Trip = await req.json()
 
-        try {
-             const trip: Trip = await req.json();
+      const supabase = createRouteHandlerClient<Database>({ cookies })
+      const {
+        data: { session }
+      } = await supabase.auth.getSession()
+      //const session2 = await getSession()
 
-            const supabase = createRouteHandlerClient<Database>({ cookies });
-            const {
-                data: { session }
-            } = await supabase.auth.getSession();
-            //const session2 = await getSession()
-
-            if (!session) {
-                return new Response(JSON.stringify({
-                    error: 'not authenticated',
-                    description: 'The user does not have an active session or is not authenticated'
-                }), { status: 500 });
-            }
-
-            if (trip.id != null) {
-                const tripID = await cancelTrip(trip.id, session.user.id);
-                if (tripID != undefined) {
-                    const response = "Trip deleted";
-                    return new Response(JSON.stringify(response), {
-                        status: 200
-                    });
-                }
-                return new Response(JSON.stringify("Trip Deleted"), {
-                    status: 200
-                });
-            }
-        } catch (err: any) {
-            return new Response(JSON.stringify({ error: { statusCode: 500, message: err.message } }));
-        }
-    } else {
-        return new Response(JSON.stringify('Method Not Allowed'));
-
+      if (!session) {
+        return new Response(
+          JSON.stringify({
+            error: "not authenticated",
+            description:
+              "The user does not have an active session or is not authenticated"
+          }),
+          { status: 500 }
+        )
+      }
+      const tripID = await cancelTrip(trip.id, session.user.id)
+      if (tripID != undefined) {
+        const response = "Trip deleted"
+        return new Response(JSON.stringify(response), {
+          status: 200
+        })
+      }
+      return new Response(JSON.stringify("Trip Deleted"), {
+        status: 200
+      })
+    } catch (err: any) {
+      return new Response(
+        JSON.stringify({ error: { statusCode: 500, message: err.message } })
+      )
     }
+  } else {
+    return new Response(JSON.stringify("Method Not Allowed"))
+  }
 }
