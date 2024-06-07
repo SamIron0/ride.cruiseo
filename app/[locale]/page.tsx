42ebed8d8d499f76a9cfe554dacad9a1af6727bc
SamIron0 <samuelironkwec@gmail.com>
2024-06-07 11:34:43 -0500

    z

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -64,7 +64,7 @@ export default function ChatPage() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke-width="1.5"
-                  stroke="currentColor"
+                  stroke="black"
                   className="size-12"
                 >
                   <path
@@ -91,7 +91,7 @@ export default function ChatPage() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke-width="1.5"
-                  stroke="currentColor"
+                  stroke="black"
                   className="size-12"
                 >
                   <path
@@ -118,7 +118,7 @@ export default function ChatPage() {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke-width="1.5"
-                  stroke="currentColor"
+                  stroke="black"
                   className="size-12"
                 >
                   <path
