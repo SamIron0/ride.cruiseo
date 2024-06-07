6622d83dc30d46fd090f0c0f34b121554534f98a
SamIron0 <samuelironkwec@gmail.com>
2024-06-07 11:31:41 -0500

    x

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -59,11 +59,20 @@ export default function ChatPage() {
           <div className=" flex justify-center">
             <div className="flex flex-col ">
               <div className="mb-4">
-                <img
-                  src="https://via.placeholder.com/64"
-                  alt="Icon 1"
-                  className="w-16 h-16"
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-16"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
+                  />
+                </svg>
               </div>
               <h2 className="text-xl font-semibold text-gray-800 mb-2">
                 Easy Booking
@@ -77,11 +86,20 @@ export default function ChatPage() {
           <div className="flex justify-center">
             <div className="flex flex-col ">
               <div className="mb-4">
-                <img
-                  src="https://via.placeholder.com/64"
-                  alt="Icon 2"
-                  className="w-16 h-16"
-                />{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-10"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+                  />
+                </svg>
               </div>
               <h2 className="text-xl font-semibold text-gray-800 mb-2">
                 Cost Sharing
@@ -95,11 +113,20 @@ export default function ChatPage() {
           <div className="flex justify-center">
             <div className="flex flex-col">
               <div className="mb-4">
-                <img
-                  src="https://via.placeholder.com/64"
-                  alt="Icon 3"
-                  className="w-16 h-16"
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-16"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
+                  />
+                </svg>
               </div>
               <h2 className="text-xl font-semibold text-gray-800 mb-2">
                 Safe and Reliable
