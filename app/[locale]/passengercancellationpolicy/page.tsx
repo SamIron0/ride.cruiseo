7825a0fd9889b7aad97e1281e8a2bbe317d28c8c
SamIron0 <samuelironkwec@gmail.com>
2024-06-08 20:25:52 -0500

    x

--- app/[locale]/passengercancellationpolicy/page.tsx
+++ app/[locale]/passengercancellationpolicy/page.tsx
@@ -15,7 +15,7 @@ export default function PassengerCancellationPolicy() {
           </Container>
         </div>
       </div>
-      <div className="max-w-2xl mx-auto bg-white p-6 my-28 text-gray-700">
+      <div className="max-w-2xl mx-auto bg-white p-6 my-28 text-gray-800">
         <h1 className="text-4xl font-bold mb-4">
           Passenger Cancellation Policy
         </h1>
