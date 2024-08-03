ff893c68c15e59e6edadf75ccc23fe6ddb47ad3c
SamIron0 <samuelironkwec@gmail.com>
2024-08-03 16:41:27 -0500

    x

--- app/[locale]/destinations/[destinationId]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react"
-
-interface RootLayoutProps {
-  children: ReactNode
-  
-}
-export default async function RootLayout({ children }: RootLayoutProps) {
-  return <> {children}</>
-}
